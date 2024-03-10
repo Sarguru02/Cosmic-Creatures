9616b9e9a1a01b1d1b408e26f96e340f1ff143d4
Sandy-274 <santhoshkalees2005@gmail.com>
2024-03-10 09:53:28 +0530

    connected to neondb

--- restapi/ecocalc/ecocalc/urls.py
+++ restapi/ecocalc/ecocalc/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for rest project.
+URL configuration for ecocalc project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
