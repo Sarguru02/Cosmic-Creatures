bb2cf4c9be7dd9d7e84b009ad0a0a3dbc46558ca
sharavanask <sharavanakrishna2004@gmail.com>
2024-03-11 13:38:15 +0530

    Updates

--- frontend/src/utils/chat.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default async function chat(message){
-    const res = await axios.post('chat/', { question: message})
-    return res.data;
-}
--- restapi/ecocalc/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from . import chatbot
-
-urlpatterns=[
-    path('',views.getData),
-    path('add/',views.addItem),
-    path('chat/', chatbot.get_gemini_response),
-]
