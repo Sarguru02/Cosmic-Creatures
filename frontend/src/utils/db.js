c4849a0d1c2cf23e6e955aec4d68383af7c36f17
Sarguru02 <psgnhealth@gmail.com>
2024-03-13 09:56:58 +0530

    database logic modified

--- frontend/src/utils/db.js
+++ frontend/src/utils/db.js
@@ -3,20 +3,22 @@
 //
 
 export function getUserData() {
-  return JSON.parse(localStorage.getItem("user"));
+    // get the current username from localstorage with the key user
+    const username = JSON.parse(localStorage.getItem("user")).username;
+    // get the full db from localstorage
+    const fulldb = JSON.parse(localStorage.getItem("database"));
+    // find the user in the db and return the user object
+    const user = fulldb.find(user => user[username]);
+    return user;
 }
+
 
 export function putUserData(props){
     const username = JSON.parse(localStorage.getItem("user")).username;
-    const userobj = {[username]: {...props}}
-    // get array of all user objects
-    const allUsers = JSON.parse(localStorage.getItem("users"));
-    // if array is empty, create new array
-    if (!allUsers) {
-        localStorage.setItem("users", JSON.stringify([userobj]));
-    } else {
-        // if array is not empty, create user object and push into array
-        allUsers.push(userobj);
-        localStorage.setItem("database", JSON.stringify(allUsers));
-    }
+    const userobj = {[username]: {fetchingData: props}};
+
+    // get the db, if it's empty, create a new one
+    const fulldb = JSON.parse(localStorage.getItem("database")) || [];
+    fulldb.push(userobj);
+    localStorage.setItem("database", JSON.stringify(fulldb));
 }
